b43e059c6ff13471f78f0102994a2f2a7e1d9204
gclover <clover.gch@gmail.com>
2013-03-15 00:48:18 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -10,21 +10,21 @@ import sys
 import glob
 import string
 import shutil
+import subprocess
 
 snappy_path = glob.glob('deps/snappy*')[0]
 leveldb_path = glob.glob('deps/leveldb*')[0]
-	
-extra_compile_args = ['-Wall', '-pedantic', '-I./%s/include/' % leveldb_path,'-shared', '-std=gnu99', '-fPIC', '-g', '-D_GNU_SOURCE']
-extra_link_args = ['-L%s' % leveldb_path, '-lleveldb', '-lpthread']
+
 
 def build_snappy():
-	print 'build_sanppy'
+	print 'build_snappy'
 	conf_cmd = 'cd %s;./configure --enable-shared=false' % snappy_path
 	make_cmd = 'cd %s;make' % snappy_path
 	os.system(conf_cmd)
 	os.system(make_cmd)
 
 def build_leveldb():
+	print 'build_leveldb'
 	srclibs = "PLATFORM_LIBS=\n"
 	destlibs = "PLATFORM_LIBS='-L../../%s/.libs'\n" % snappy_path
 	detect_file = '%s/build_detect_platform' % leveldb_path
@@ -36,7 +36,8 @@ def build_leveldb():
 	os.system(make_cmd)
 	libso = glob.glob('deps/leveldb*/libleveldb.so.*')[0]
 	destlibso = glob.glob('deps/leveldb*/')[0] + '/libleveldb.so'
-	shutil.copy(libso, destlibso)
+	#os.unlink(destlibso)
+	#shutil.copy(libso, destlibso)
 
 def main():
 	if len(sys.argv) >= 2 and sys.argv[1] == 'build_deps':
@@ -44,31 +45,43 @@ def main():
 		build_leveldb()
 		return
 
+    	print 'deleting generated files...'
+	os.system('rm -rf build')
+	os.system('rm -rf ext/leveldb.cpp')
+
+	extra_compile_args = ['-I./%s/include/' % leveldb_path, '-shared']
+	extra_link_args = ['-L%s' % leveldb_path, '-lleveldb', '-lpthread', '-Wl,--rpath=.']
+
+	print '>>'
+	print find_packages()
+	print '<<'
 	setup(
-	name = "leveldb",
-	version = '0.0.1',
-	namespace_packages=[],
-	packages = find_packages(),
-	#zip_safe = False,
+		name = "leveldb",
+		version = '0.0.1',
+		namespace_packages=[],
+		packages = find_packages(),
+		#zip_safe = False,
 
-	description = "leveldb python binding",
-	author = "gclover",
-	author_email = "clover.gch@gmail.com",
+		data_files = [('', glob.glob('deps/leveldb*/libleveldb.so*'))],
 
-	license = "",
-	keywords = (),
-	platforms = "Independant",
-	url = "",
+		description = "leveldb python binding",
+		author = "gclover",
+		author_email = "clover.gch@gmail.com",
 
-	cmdclass = {'build_ext' : build_ext},
-	ext_modules = [
-		Extension('leveldb', 
-			['ext/leveldb.pyx'],
-			language = 'c++',
-			extra_compile_args = extra_compile_args,
-			extra_link_args = extra_link_args
-		)
-	]
+		license = "",
+		keywords = (),
+		platforms = "Independant",
+		url = "",
+
+		cmdclass = {'build_ext' : build_ext},
+		ext_modules = [
+			Extension('leveldb', 
+				['ext/leveldb.pyx'] + glob.glob('ext/*.cpp'),
+				language = 'c++',
+				extra_compile_args = extra_compile_args,
+				extra_link_args = extra_link_args
+			)
+		]
 	)
 
 if __name__ == '__main__':
